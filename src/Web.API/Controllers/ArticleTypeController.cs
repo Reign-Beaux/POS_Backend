658601b9a77984555f708890068fbf6f390a6762
Reign-Beaux <samc920130@hotmail.com>
2025-04-07 23:22:58 -0600

    CRUD Funcionando

--- src/Web.API/Controllers/ArticleTypeController.cs
+++ src/Web.API/Controllers/ArticleTypeController.cs
@@ -1,5 +1,8 @@
 ﻿using Application.UseCases.ArticleTypes.Commands.Create;
+using Application.UseCases.ArticleTypes.Commands.Delete;
+using Application.UseCases.ArticleTypes.Commands.Update;
 using Application.UseCases.ArticleTypes.Queries.GetAll;
+using Application.UseCases.ArticleTypes.Queries.GetById;
 using Domain.Entities.ArticleTypes;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -22,11 +25,39 @@ namespace Web.API.Controllers
             return HandleResponse(operationResult);
         }
 
+        [HttpGet(routeTemplateId)]
+        [ProducesResponseType(typeof(ArticleType), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            ArticleTypeGetByIdQuery query = new(id);
+            var operationResult = await _mediator.Send(query);
+            return HandleResponse(operationResult);
+        }
+
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
         public async Task<IActionResult> Create([FromBody] ArticleTypeCreateCommand command)
         {
             var operationResult = await _mediator.Send(command);
+            return HandleResponse(operationResult);
+        }
+
+        [HttpPut]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> Update([FromBody] ArticleTypeUpdateCommand command)
+        {
+            var operationResult = await _mediator.Send(command);
+            return HandleResponse(operationResult);
+        }
+
+        [HttpDelete(routeTemplateId)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var operationResult = await _mediator.Send(new ArticleTypeDeleteCommand(id));
             return HandleResponse(operationResult);
         }
     }
