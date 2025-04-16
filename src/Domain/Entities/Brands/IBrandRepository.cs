9482aa3fbe827100d106f52311fd6afec78f993d
Reign-Beaux <samc920130@hotmail.com>
2025-04-16 01:45:42 -0600

    Se agrega la ionterface para los repositories

---
+++ src/Domain/Entities/Brands/IBrandRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities.Brands
+{
+    public interface IBrandRepository
+    {
+        Task<IEnumerable<Brand>> GetAll();
+        Task<Brand?> GetById(Guid id);
+        Task<Brand?> GetByName(string name);
+        void Add(Brand brand, CancellationToken cancellationToken = default);
+        void Update(Brand brand, CancellationToken cancellationToken = default);
+        void Delete(Brand brand, CancellationToken cancellationToken = default);
+    }
+}
