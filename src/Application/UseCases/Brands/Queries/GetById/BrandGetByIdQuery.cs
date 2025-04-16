6c5cba82bcd650dd9f16b8578ad77203f0fb7e3d
Reign-Beaux <samc920130@hotmail.com>
2025-04-16 01:54:47 -0600

    Se creo el caso de uso para opbtener por Id

---
+++ src/Application/UseCases/Brands/Queries/GetById/BrandGetByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.DTOs.Brands;
+using Application.OperationResults;
+using MediatR;
+
+namespace Application.UseCases.Brands.Queries.GetById
+{
+    public record BrandGetByIdQuery(Guid Id) : IRequest<OperationResult<BrandDTO>>;
+}
---
+++ src/Application/UseCases/Brands/Queries/GetById/BrandGetByIdValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.UseCases.Brands.Queries.GetById
+{
+    public class BrandGetByIdValidator : AbstractValidator<BrandGetByIdQuery>
+    {
+        public BrandGetByIdValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("Brand ID cannot be empty.");
+        }
+    }
+}
