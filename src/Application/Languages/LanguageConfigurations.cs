96f710bf4b12642ca702a08508d2c493ea00e47b
Reign-Beaux <samc920130@hotmail.com>
2025-04-24 01:20:17 -0600

    Se liminan resources debido a que se ocupa base de datos y cache

--- src/Application/Languages/LanguageConfigurations.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Application.Languages
-{
-    public static class LanguageConfigurations
-    {
-        public readonly static string[] SupportedLanguages = ["en", "es"];
-        public readonly static string DefaultLanguage = "en";
-    }
-}
