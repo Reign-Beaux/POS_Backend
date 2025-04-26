9e9cd6ef59e192dbb7c22fef97375293fa1d7aae
Reign-Beaux <samc920130@hotmail.com>
2025-04-26 03:58:38 -0600

    Se agregan nuevas constantes

---
+++ src/Application/Constants/CachedKeys/CatalogCachedKeys.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Constants.CachedKeys
+{
+    public static class CatalogCachedKeys
+    {
+        public const string NameIsRequired = "CatalogNameIsRequired";
+        public const string DescriptionIsRequired = "CatalogDescriptionIsRequired";
+        public const string NameMaxLength = "CatalogNameMaxLength";
+        public const string DescriptionMaxLength = "CatalogDescriptionMaxLength";
+    }
+}
---
+++ src/Application/Constants/LanguageConfigurations.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Constants
+{
+    public static class LanguageConfigurations
+    {
+        public const string DefaultLanguage = "en";
+        public static readonly string[] SupportedLanguages = ["en", "es"];
+    }
+}
