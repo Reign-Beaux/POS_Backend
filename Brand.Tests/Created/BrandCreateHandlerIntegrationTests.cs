ca45288c7e69c9cc503186c1d1e5ab78019ad2f7
Reign-Beaux <samc920130@hotmail.com>
2025-04-26 22:25:10 -0600

    Cambios

---
+++ Brand.Tests/Created/BrandCreateHandlerIntegrationTests.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Brand.Tests.Created
+{
+    public class BrandCreateHandlerIntegrationTests : IClassFixture<WebApplicationFactory<Web.API.Program>>
+    {
+    }
+}
---
+++ Brand.Tests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using AutoMapper;
+global using FluentAssertions;
+global using FluentValidation;
+global using MediatR;
+global using Microsoft.Extensions.DependencyInjection;
+global using Moq;
+global using Microsoft.Extensions.Logging;
+global using System.Net;
