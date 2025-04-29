55830a70f979844b7e462014222b5600ec291b51
Reign-Beaux <samc920130@hotmail.com>
2025-04-29 00:18:02 -0600

    Se agrega tipado para factory

--- test/BaseTests/Factories/POSWebApplicationFactory.cs
+++ test/BaseTests/Factories/POSWebApplicationFactory.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Infrastructure.Persistence.Context;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Web.API;
 
@@ -8,37 +11,26 @@ namespace BaseTests.Factories
     {
         protected override IHost CreateHost(IHostBuilder builder)
         {
-            // Aquí puedes sobrescribir servicios para las pruebas si es necesario.
-            // Por ejemplo, reemplazar la base de datos real con una en memoria:
-            /*
-            builder.ConfigureServices(services =>
-            {
-                // Elimina la configuración de DbContext de la aplicación principal
-                var descriptor = services.SingleOrDefault(
-                    d => d.ServiceType ==
-                        typeof(DbContextOptions<ApplicationDbContext>)); // Reemplaza ApplicationDbContext con tu DbContext
+            //builder.ConfigureServices(services =>
+            //{
+            //    // Quita el DbContext registrado previamente (el que va a la BD real)
+            //    var descriptor = services.SingleOrDefault(
+            //        d => d.ServiceType == typeof(DbContextOptions<PosDbContext>));
+            //    if (descriptor != null)
+            //        services.Remove(descriptor);
 
-                if (descriptor != null)
-                {
-                    services.Remove(descriptor);
-                }
+            //    // Agrega el DbContext usando InMemory
+            //    services.AddDbContext<PosDbContext>(options =>
+            //    {
+            //        options.UseInMemoryDatabase("TestDb_" + Guid.NewGuid()); // Asegura aislamiento entre pruebas
+            //    });
 
-                // Agrega un DbContext que use una base de datos en memoria
-                services.AddDbContext<ApplicationDbContext>(options => // Reemplaza ApplicationDbContext con tu DbContext
-                {
-                    options.UseInMemoryDatabase("InMemoryDbForTesting");
-                });
-
-                // Opcional: Asegúrate de que la base de datos en memoria se cree
-                var sp = services.BuildServiceProvider();
-                using (var scope = sp.CreateScope())
-                {
-                    var scopedServices = scope.ServiceProvider;
-                    var db = scopedServices.GetRequiredService<ApplicationDbContext>(); // Reemplaza ApplicationDbContext con tu DbContext
-                    db.Database.EnsureCreated();
-                }
-            });
-            */
+            //    // Opcional: asegurarse de crear la BD
+            //    var sp = services.BuildServiceProvider();
+            //    using var scope = sp.CreateScope();
+            //    var db = scope.ServiceProvider.GetRequiredService<PosDbContext>();
+            //    db.Database.EnsureCreated();
+            //});
 
             return base.CreateHost(builder);
         }
