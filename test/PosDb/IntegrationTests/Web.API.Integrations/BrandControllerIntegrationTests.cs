cb2c54eeef071a5ba3981e88b9f79db236ec3df6
Reign-Beaux <samc920130@hotmail.com>
2025-04-28 20:14:47 -0600

    Correcciones en pruebas

---
+++ test/PosDb/IntegrationTests/Web.API.Integrations/BrandControllerIntegrationTests.cs
@@ -0,0 +1,44 @@
+﻿using Application.Features.Brands.DTOs;
+using Application.Features.Brands.UseCases.Commands.Create;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Web.API.Integrations
+{
+    public class BrandControllerIntegrationTests(POSWebApplicationFactory factory) : IClassFixture<POSWebApplicationFactory>
+    {
+        private readonly HttpClient _client = factory.CreateClient();
+
+        [Fact]
+        public async Task Post_Brand_WithValidData_ShouldReturnCreated()
+        {
+            // Arrange
+            var command = new BrandCreateCommand("Nueva Marca Test", "Descripción de prueba");
+            const string requestUri = "/api/Brand";
+
+            // Act
+            HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            var location = response.Headers.Location;
+            location.Should().NotBeNull();
+            location!.AbsolutePath.Should().MatchRegex(@"^/api/Brand/[0-9a-fA-F\-]{36}$");
+
+            var createdBrandId = await response.Content.ReadFromJsonAsync<Guid>();
+            createdBrandId.Should().NotBeEmpty();
+
+            // Confirmar que el recurso efectivamente existe
+            var getResponse = await _client.GetAsync($"{requestUri}/{createdBrandId}");
+            getResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var brand = await getResponse.Content.ReadFromJsonAsync<BrandDTO>();
+            brand.Should().NotBeNull();
+            brand!.Id.Should().Be(createdBrandId);
+            brand.Name.Should().Be(command.Name);
+            brand.Description.Should().Be(command.Description);
+        }
+    }
+}
---
+++ test/PosDb/IntegrationTests/Web.API.Integrations/POSWebApplicationFactory.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+
+namespace Web.API.Integrations
+{
+    public class POSWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            // Aquí puedes sobrescribir servicios para las pruebas si es necesario.
+            // Por ejemplo, reemplazar la base de datos real con una en memoria:
+            /*
+            builder.ConfigureServices(services =>
+            {
+                // Elimina la configuración de DbContext de la aplicación principal
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<ApplicationDbContext>)); // Reemplaza ApplicationDbContext con tu DbContext
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                // Agrega un DbContext que use una base de datos en memoria
+                services.AddDbContext<ApplicationDbContext>(options => // Reemplaza ApplicationDbContext con tu DbContext
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+
+                // Opcional: Asegúrate de que la base de datos en memoria se cree
+                var sp = services.BuildServiceProvider();
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<ApplicationDbContext>(); // Reemplaza ApplicationDbContext con tu DbContext
+                    db.Database.EnsureCreated();
+                }
+            });
+            */
+
+            return base.CreateHost(builder);
+        }
+    }
+}
