cc661d8607a06a7ec0c1f89e19949e5a0108869a
Reign-Beaux <samc920130@hotmail.com>
2025-05-01 01:47:37 -0600

    Mejoras usando la base de datos de pruebas

--- test/BaseTests/Factories/POSWebApplicationFactory.cs
+++ test/BaseTests/Factories/POSWebApplicationFactory.cs
@@ -11,27 +11,6 @@ namespace BaseTests.Factories
     {
         protected override IHost CreateHost(IHostBuilder builder)
         {
-            //builder.ConfigureServices(services =>
-            //{
-            //    // Quita el DbContext registrado previamente (el que va a la BD real)
-            //    var descriptor = services.SingleOrDefault(
-            //        d => d.ServiceType == typeof(DbContextOptions<PosDbContext>));
-            //    if (descriptor != null)
-            //        services.Remove(descriptor);
-
-            //    // Agrega el DbContext usando InMemory
-            //    services.AddDbContext<PosDbContext>(options =>
-            //    {
-            //        options.UseInMemoryDatabase("TestDb_" + Guid.NewGuid()); // Asegura aislamiento entre pruebas
-            //    });
-
-            //    // Opcional: asegurarse de crear la BD
-            //    var sp = services.BuildServiceProvider();
-            //    using var scope = sp.CreateScope();
-            //    var db = scope.ServiceProvider.GetRequiredService<PosDbContext>();
-            //    db.Database.EnsureCreated();
-            //});
-
             return base.CreateHost(builder);
         }
     }
