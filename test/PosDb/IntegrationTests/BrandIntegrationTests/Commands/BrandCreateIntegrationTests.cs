7dce0c0f3efb6ed30818ead9f341795f74f47d01
Reign-Beaux <samc920130@hotmail.com>
2025-05-06 01:29:25 -0600

    Se terminan las pruebas de integración

--- test/PosDb/IntegrationTests/BrandIntegrationTests/Commands/BrandCreateIntegrationTests.cs
+++ test/PosDb/IntegrationTests/BrandIntegrationTests/Commands/BrandCreateIntegrationTests.cs
@@ -1,190 +1,253 @@
-﻿using BaseTests.Factories;
+﻿using Application.Behaviors;
+using Application.Features.Brands.DTOs;
+using Application.Features.Brands.UseCases.Commands.Create;
+using Application.OperationResults;
+using BaseTests.Factories;
+using Domain.Entities;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
 
 namespace BrandIntegrationTests.Commands
 {
     public class BrandCreateIntegrationTests(POSWebApplicationFactory factory) : IClassFixture<POSWebApplicationFactory>
     {
-        //private readonly HttpClient _client = factory.CreateClient();
+        private readonly HttpClient _client = factory.CreateClient();
 
-        //[Fact]
-        //public async Task WhenAllIsOk_ShouldReturnCreated()
-        //{
-        //    // Arrange
-        //    var command = new BrandCreateCommand($"Test name {Guid.NewGuid()}", "Descripción de prueba");
-        //    const string requestUri = "/api/Brand";
+        [Fact]
+        public async Task WhenAllIsOk_ShouldReturnCreated()
+        {
+            // Arrange
+            var command = new BrandCreateCommand()
+            {
+                Name = $"Test name {Guid.NewGuid()}",
+                Description = "Test description"
+            };
+            const string requestUri = "/api/Brand";
 
-        //    // Act
-        //    HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
+            // Act
+            HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
 
-        //    // Assert
-        //    response.StatusCode.Should().Be(HttpStatusCode.Created);
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
 
-        //    var location = response.Headers.Location;
-        //    location.Should().NotBeNull();
-        //    location!.AbsolutePath.Should().MatchRegex(@"^/api/Brand/[0-9a-fA-F\-]{36}$");
+            var location = response.Headers.Location;
+            location.Should().NotBeNull();
+            location!.AbsolutePath.Should().MatchRegex(@"^/api/Brand/[0-9a-fA-F\-]{36}$");
 
-        //    var createdBrandId = await response.Content.ReadFromJsonAsync<Guid>();
-        //    createdBrandId.Should().NotBeEmpty();
+            var createdBrandId = await response.Content.ReadFromJsonAsync<Guid>();
+            createdBrandId.Should().NotBeEmpty();
 
-        //    // Additional Act
-        //    var getResponse = await _client.GetAsync($"{requestUri}/{createdBrandId}");
+            // Additional Act
+            var getResponse = await _client.GetAsync($"{requestUri}/{createdBrandId}");
 
-        //    // Additional Assert
-        //    getResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            // Additional Assert
+            getResponse.StatusCode.Should().Be(HttpStatusCode.OK);
 
-        //    var brand = await getResponse.Content.ReadFromJsonAsync<BrandDTO>();
-        //    brand.Should().NotBeNull();
-        //    brand.Id.Should().Be(createdBrandId);
-        //    brand.Name.Should().Be(command.Name);
-        //    brand.Description.Should().Be(command.Description);
-        //}
+            var brand = await getResponse.Content.ReadFromJsonAsync<BrandDTO>();
+            brand.Should().NotBeNull();
+            brand.Id.Should().Be(createdBrandId);
+            brand.Name.Should().Be(command.Name);
+            brand.Description.Should().Be(command.Description);
+        }
 
-        //[Fact]
-        //public async Task WhenNameIsEmpty_ShouldReturnBadRequest()
-        //{
-        //    // Arrange
-        //    var command = new BrandCreateCommand(string.Empty, "Descripción de prueba");
-        //    const string requestUri = "/api/Brand";
+        [Fact]
+        public async Task WhenNameIsEmpty_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var command = new BrandCreateCommand()
+            {
+                Name = string.Empty,
+                Description = "Test description"
+            };
+            const string requestUri = "/api/Brand";
 
-        //    // Act
-        //    HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
+            // Act
+            HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
 
-        //    // Assert
-        //    response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        //    var result = await response.Content.ReadFromJsonAsync<ErrorDetails>();
-        //    result.Should().NotBeNull();
-        //    result.Title.Should().Be(ValidationMessages.Title);
-        //    result.Message.Should().Be(ValidationMessages.Message);
-        //    result.Errors.Should().NotBeNull();
-        //    result.Errors.Should().ContainSingle(e =>
-        //        e.PropertyName == nameof(BrandCreateCommand.Name) &&
-        //        e.ErrorMessage != string.Empty);
-        //}
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var result = await response.Content.ReadFromJsonAsync<ErrorDetails>();
+            result.Should().NotBeNull();
+            result.Title.Should().Be(ValidationMessages.Title);
+            result.Message.Should().Be(ValidationMessages.Message);
+            result.Errors.Should().NotBeNull();
+            result.Errors.Should().ContainSingle(e =>
+                e.PropertyName == nameof(BrandCreateCommand.Name) &&
+                e.ErrorMessage != string.Empty);
+        }
 
-        //[Fact]
-        //public async Task WhenNameExceedsMaxLength_ShouldReturnValidationError()
-        //{
-        //    // Arrange
-        //    string NameTooLong = new('A', BaseCatalog.NameMaxLength + 1);
-        //    var command = new BrandCreateCommand(NameTooLong, "Descripción de prueba");
-        //    const string requestUri = "/api/Brand";
+        [Fact]
+        public async Task WhenNameExceedsMaxLength_ShouldReturnValidationError()
+        {
+            // Arrange
+            string NameTooLong = new('A', BaseCatalog.NameMaxLength + 1);
+            var command = new BrandCreateCommand()
+            {
+                Name = NameTooLong,
+                Description = "Test description"
+            };
+            const string requestUri = "/api/Brand";
 
-        //    // Act
-        //    HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
+            // Act
+            HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
 
-        //    // Assert
-        //    response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        //    var result = await response.Content.ReadFromJsonAsync<ErrorDetails>();
-        //    result.Should().NotBeNull();
-        //    result.Title.Should().Be(ValidationMessages.Title);
-        //    result.Message.Should().Be(ValidationMessages.Message);
-        //    result.Errors.Should().NotBeNull();
-        //    result.Errors.Should().ContainSingle(e =>
-        //        e.PropertyName == nameof(BrandCreateCommand.Name) &&
-        //        e.ErrorMessage != string.Empty);
-        //}
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var result = await response.Content.ReadFromJsonAsync<ErrorDetails>();
+            result.Should().NotBeNull();
+            result.Title.Should().Be(ValidationMessages.Title);
+            result.Message.Should().Be(ValidationMessages.Message);
+            result.Errors.Should().NotBeNull();
+            result.Errors.Should().ContainSingle(e =>
+                e.PropertyName == nameof(BrandCreateCommand.Name) &&
+                e.ErrorMessage != string.Empty);
+        }
 
-        //[Fact]
-        //public async Task WhenDescriptionIsEmpty_ShouldReturnBadRequest()
-        //{
-        //    // Arrange
-        //    var command = new BrandCreateCommand("Test name", string.Empty);
-        //    const string requestUri = "/api/Brand";
+        [Fact]
+        public async Task WhenDescriptionIsEmpty_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var command = new BrandCreateCommand()
+            {
+                Name = "Test name",
+                Description = string.Empty
+            };
+            const string requestUri = "/api/Brand";
 
-        //    // Act
-        //    HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
+            // Act
+            HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
 
-        //    // Assert
-        //    response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        //    var result = await response.Content.ReadFromJsonAsync<ErrorDetails>();
-        //    result.Should().NotBeNull();
-        //    result.Title.Should().Be(ValidationMessages.Title);
-        //    result.Message.Should().Be(ValidationMessages.Message);
-        //    result.Errors.Should().NotBeNull();
-        //    result.Errors.Should().ContainSingle(e =>
-        //        e.PropertyName == nameof(BrandCreateCommand.Description) &&
-        //        e.ErrorMessage != string.Empty);
-        //}
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var result = await response.Content.ReadFromJsonAsync<ErrorDetails>();
+            result.Should().NotBeNull();
+            result.Title.Should().Be(ValidationMessages.Title);
+            result.Message.Should().Be(ValidationMessages.Message);
+            result.Errors.Should().NotBeNull();
+            result.Errors.Should().ContainSingle(e =>
+                e.PropertyName == nameof(BrandCreateCommand.Description) &&
+                e.ErrorMessage != string.Empty);
+        }
 
-        //[Fact]
-        //public async Task WhenDescriptionExceedsMaxLength_ShouldReturnValidationError()
-        //{
-        //    // Arrange
-        //    string DescriptionTooLong = new('A', BaseCatalog.DescriptionMaxLength + 1);
-        //    var command = new BrandCreateCommand("Test name", DescriptionTooLong);
-        //    const string requestUri = "/api/Brand";
+        [Fact]
+        public async Task WhenDescriptionExceedsMaxLength_ShouldReturnValidationError()
+        {
+            // Arrange
+            string DescriptionTooLong = new('A', BaseCatalog.DescriptionMaxLength + 1);
+            var command = new BrandCreateCommand()
+            {
+                Name = "Test name",
+                Description = DescriptionTooLong
+            };
+            const string requestUri = "/api/Brand";
 
-        //    // Act
-        //    HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
+            // Act
+            HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
 
-        //    // Assert
-        //    response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        //    var result = await response.Content.ReadFromJsonAsync<ErrorDetails>();
-        //    result.Should().NotBeNull();
-        //    result.Title.Should().Be(ValidationMessages.Title);
-        //    result.Message.Should().Be(ValidationMessages.Message);
-        //    result.Errors.Should().NotBeNull();
-        //    result.Errors.Should().ContainSingle(e =>
-        //        e.PropertyName == nameof(BrandCreateCommand.Description) &&
-        //        e.ErrorMessage != string.Empty);
-        //}
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var result = await response.Content.ReadFromJsonAsync<ErrorDetails>();
+            result.Should().NotBeNull();
+            result.Title.Should().Be(ValidationMessages.Title);
+            result.Message.Should().Be(ValidationMessages.Message);
+            result.Errors.Should().NotBeNull();
+            result.Errors.Should().ContainSingle(e =>
+                e.PropertyName == nameof(BrandCreateCommand.Description) &&
+                e.ErrorMessage != string.Empty);
+        }
 
-        //[Fact]
-        //public async Task WhenAllFieldsAreEmpty_ShouldReturnValidationErrors()
-        //{
-        //    // Arrange
-        //    var command = new BrandCreateCommand(string.Empty, string.Empty);
-        //    const string requestUri = "/api/Brand";
+        [Fact]
+        public async Task WhenAllFieldsAreEmpty_ShouldReturnValidationErrors()
+        {
+            // Arrange
+            var command = new BrandCreateCommand()
+            {
+                Name = string.Empty,
+                Description = string.Empty
+            };
+            const string requestUri = "/api/Brand";
 
-        //    // Act
-        //    HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
+            // Act
+            HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
 
-        //    // Assert
-        //    response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        //    var result = await response.Content.ReadFromJsonAsync<ErrorDetails>();
-        //    result.Should().NotBeNull();
-        //    result.Title.Should().Be(ValidationMessages.Title);
-        //    result.Message.Should().Be(ValidationMessages.Message);
-        //    result.Errors.Should().NotBeNull();
-        //    result.Errors.Should().Contain(e =>
-        //        e.PropertyName == nameof(BrandCreateCommand.Name) &&
-        //        e.ErrorMessage != string.Empty);
-        //    result.Errors.Should().Contain(e =>
-        //        e.PropertyName == nameof(BrandCreateCommand.Description) &&
-        //        e.ErrorMessage != string.Empty);
-        //}
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var result = await response.Content.ReadFromJsonAsync<ErrorDetails>();
+            result.Should().NotBeNull();
+            result.Title.Should().Be(ValidationMessages.Title);
+            result.Message.Should().Be(ValidationMessages.Message);
+            result.Errors.Should().NotBeNull();
+            result.Errors.Should().Contain(e =>
+                e.PropertyName == nameof(BrandCreateCommand.Name) &&
+                e.ErrorMessage != string.Empty);
+            result.Errors.Should().Contain(e =>
+                e.PropertyName == nameof(BrandCreateCommand.Description) &&
+                e.ErrorMessage != string.Empty);
+        }
 
-        //[Fact]
-        //public async Task WhenAllFieldsExceedMaxLength_ShouldReturnValidationErrors()
-        //{
-        //    // Arrange
-        //    string NameTooLong = new('A', BaseCatalog.NameMaxLength + 1);
-        //    string DescriptionTooLong = new('A', BaseCatalog.DescriptionMaxLength + 1);
-        //    var command = new BrandCreateCommand(NameTooLong, DescriptionTooLong);
-        //    const string requestUri = "/api/Brand";
+        [Fact]
+        public async Task WhenAllFieldsExceedMaxLength_ShouldReturnValidationErrors()
+        {
+            // Arrange
+            string NameTooLong = new('A', BaseCatalog.NameMaxLength + 1);
+            string DescriptionTooLong = new('A', BaseCatalog.DescriptionMaxLength + 1);
+            var command = new BrandCreateCommand()
+            {
+                Name = NameTooLong,
+                Description = DescriptionTooLong
+            };
+            const string requestUri = "/api/Brand";
 
-        //    // Act
-        //    HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
+            // Act
+            HttpResponseMessage response = await _client.PostAsJsonAsync(requestUri, command);
 
-        //    // Assert
-        //    response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        //    var result = await response.Content.ReadFromJsonAsync<ErrorDetails>();
-        //    result.Should().NotBeNull();
-        //    result.Title.Should().Be(ValidationMessages.Title);
-        //    result.Message.Should().Be(ValidationMessages.Message);
-        //    result.Errors.Should().NotBeNull();
-        //    result.Errors.Should().Contain(e =>
-        //        e.PropertyName == nameof(BrandCreateCommand.Name) &&
-        //        e.ErrorMessage != string.Empty);
-        //    result.Errors.Should().Contain(e =>
-        //        e.PropertyName == nameof(BrandCreateCommand.Description) &&
-        //        e.ErrorMessage != string.Empty);
-        //}
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var result = await response.Content.ReadFromJsonAsync<ErrorDetails>();
+            result.Should().NotBeNull();
+            result.Title.Should().Be(ValidationMessages.Title);
+            result.Message.Should().Be(ValidationMessages.Message);
+            result.Errors.Should().NotBeNull();
+            result.Errors.Should().Contain(e =>
+                e.PropertyName == nameof(BrandCreateCommand.Name) &&
+                e.ErrorMessage != string.Empty);
+            result.Errors.Should().Contain(e =>
+                e.PropertyName == nameof(BrandCreateCommand.Description) &&
+                e.ErrorMessage != string.Empty);
+        }
 
-        //[Fact]
-        //public async Task WhenBrandAlreadyExists_ShouldReturnConflict()
-        //{
-         
-        //}
+        [Fact]
+        public async Task WhenBrandAlreadyExists_ShouldReturnConflict()
+        {
+            // Arrange: Obtener todos los brands
+            const string requestUri = "/api/Brand";
+            var getAllResponse = await _client.GetAsync(requestUri);
+            getAllResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var brands = await getAllResponse.Content.ReadFromJsonAsync<List<BrandDTO>>();
+            brands.Should().NotBeNull();
+            brands!.Count.Should().BeGreaterThan(0);
+
+            var existingBrand = brands.First();
+
+            var duplicateCommand = new BrandCreateCommand()
+            {
+                Name = existingBrand.Name,
+                Description = existingBrand.Description
+            };
+
+            // Act: Intentar crear una marca con los mismos datos
+            var postResponse = await _client.PostAsJsonAsync(requestUri, duplicateCommand);
+
+            // Assert
+            postResponse.StatusCode.Should().Be(HttpStatusCode.Conflict);
+
+            var error = await postResponse.Content.ReadFromJsonAsync<ErrorDetails>();
+            error.Should().NotBeNull();
+            error!.Title.Should().NotBeNullOrEmpty();
+            error.Message.Should().NotBeNullOrEmpty();
+            error.Errors.Should().BeNull();
+        }
     }
 }
